46886e50f4de576efb96a4b31058b9b15071d27e
Karlik <kkkj91@yahoo.pl>
2019-05-14 15:16:34 +0200

    route for user's recipes' list

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -8,11 +8,22 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProfileController extends AbstractController
 {
     /**
-     * @Route("/profile/data", name="profileData")
+     * @Route("/profile/data", name="app_profile_data")
      */
     public function profileData()
     {
         return $this->render('profile/data.html.twig', [
         ]);
     }
+
+    /**
+     * @Route("/profile/recipes/list", name="app_profile_recipe_list")
+     */
+    public function profileRecipeList()
+    {
+        //@TODO form list of current user recipes
+        return $this->render('profile/recipeList.html.twig', [
+        ]);
+    }
+
 }
