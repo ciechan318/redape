594404cd75586e8bbbd8c9bba3d00ab874a26788
Karlik <kkkj91@yahoo.pl>
2019-05-15 11:58:43 +0200

    added boostrap; ProfileData edit form

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Form\ProfileDataType;
+use App\Service\ClientManager;
+use App\Service\FlashManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfileController extends AbstractController
@@ -10,9 +14,29 @@ class ProfileController extends AbstractController
     /**
      * @Route("/profile/data", name="app_profile_data")
      */
-    public function profileData()
+    public function profileData(Request $request, ClientManager $clientManager)
     {
+        $form = $this->createForm(ProfileDataType::class, $clientManager->getUser());
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $clientManager->saveUser($form->getData());
+
+            $this->addFlash(FlashManager::FLASH_TYPE_SUCCESS,FlashManager::FLASH_MESSAGE_FORM_DATA_SAVED);
+        }
+
         return $this->render('profile/data.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/profile/change-password", name="app_profile_password")
+     */
+    public function profilePassword()
+    {
+        return $this->render('profile/passwordChange.html.twig', [
         ]);
     }
 
@@ -21,8 +45,28 @@ class ProfileController extends AbstractController
      */
     public function profileRecipeList()
     {
-        //@TODO form list of current user recipes
+        //@TODO list of current user's recipes
         return $this->render('profile/recipeList.html.twig', [
+        ]);
+    }
+
+    /**
+     * @Route("/profile/recipes/create", name="app_profile_recipe_create")
+     */
+    public function profileRecipeCreate()
+    {
+        //@TODO form create recipe
+        return $this->render('profile/recipeCreate.html.twig', [
+        ]);
+    }
+
+    /**
+     * @Route("/profile/recipes/edit", name="app_profile_recipe_edit")
+     */
+    public function profileRecipeEdit()
+    {
+        //@TODO form edit recipe
+        return $this->render('profile/recipeEdit.html.twig', [
         ]);
     }
 
--- src/Service/ClientManager.php
+++ src/Service/ClientManager.php
@@ -3,6 +3,7 @@
 namespace App\Service;
 
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Security\Core\Security;
 
 
@@ -14,13 +15,25 @@ class ClientManager
      */
     private $security;
 
-    public function __construct(Security $security)
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    public function __construct(Security $security, EntityManagerInterface $entityManager)
     {
         $this->security = $security;
+        $this->entityManager = $entityManager;
     }
 
     public function getUser(): ?User
     {
         return $this->security->getUser();
+    }
+
+    public function saveUser(User $user): void
+    {
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
     }
 }
