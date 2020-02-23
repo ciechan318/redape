e235ec7c27e7c5cbedcdc8d2cf4fee6fa76af24d
Karlik <kkkj91@yahoo.pl>
2020-02-23 14:09:32 +0100

    Upgraded code to newest dependencies

--- src/Repository/RecipeImageRepository.php
+++ src/Repository/RecipeImageRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\RecipeImage;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method RecipeImage|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +14,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class RecipeImageRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, RecipeImage::class);
     }
